25960f0e1c27c50ddd795452e2f593219f0fa637
minsau <minsau2@gmail.com>
2017-02-05 22:49:58 -0600

    Otro cambio

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const http = require('http');
 const port = process.env.PORT || 8080;
+const fs = require('fs');
 
 const server = http.createServer();
 
@@ -14,7 +15,13 @@ server.listen(port);
 // A partir de aqui van todas las funciones 
 
 function onRequest(req, res){
-  res.end('Hola io.js\n');
+  fs.readFile('public/index.html', function(err, file){
+    if(err){
+      //console.log("Hubo error");
+      return res.end(err.message);
+    }
+    res.end(file);
+  });
 }
 
 function onListening() {
