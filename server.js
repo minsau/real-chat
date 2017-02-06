deda3e0c47cd60b74b050699897f9550e333364e
minsau <minsau2@gmail.com>
2017-02-05 22:31:31 -0600

    Cuarto commit

--- server.js
+++ server.js
@@ -3,11 +3,17 @@
 const http = require('http');
 const port = process.env.PORT || 8080;
 
-const server = http.createServer(
-(req, res) => {
-  res.end('Hola io.js');
-});
+const server = http.createServer(onRequest);
 
-server.listen(port, () => {
+server.listen(port, onListening);
+
+
+// a partir de aqui van todas las funciones 
+
+function onRequest(req, res){
+  res.end('Hola io.js\n');
+}
+
+function onListening() {
   console.log('Servidor escuchando en ' + port);
-});
+}
